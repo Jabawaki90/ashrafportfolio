56f014c36b4ff06af085a92fd6791b47ad868b81
Ashraf Azemi <ashraf.chzm@gmail.com>
2021-02-16 23:05:54 +0800

    1st attempt

---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Card.css";
+
+const Card = () => {
+  return (
+    <div className="card">
+      <img src="../images/img-1.jpg" alt="no pictu" />
+      <img src="../images/img-2.jpg" alt="no pictu" />
+    </div>
+  );
+};
+
+export default Card;
