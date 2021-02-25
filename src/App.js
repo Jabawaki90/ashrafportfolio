6c7b1144a18ce9cf3a23b700e36681fab69a509c
Ashraf Azemi <ashraf.chzm@gmail.com>
2021-02-25 23:50:01 +0800

    layout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import React from "react";
 import "./App.css";
-import Card from "./components/Card";
 
 function App() {
   return (
-    <main>
-      <header>Ashraf Azemi Portfolio</header>
-      <div className="project">
-        <Card />
+    <>
+      <div className="main">
+        <div className="header">
+          <div className="leftheader">
+            <div className="portfolio">
+              <text>Portfolio Projects</text>
+            </div>
+            <div className="contact">
+              <text>Contact</text>
+            </div>
+          </div>
+          <div className="rightheader">
+            <div className="mystory">
+              <text>My Story</text>
+            </div>
+            <div className="personalparticular">
+              <text>Personal Particular</text>
+            </div>
+            <div className="experience">
+              <text>Working Experience</text>
+            </div>
+          </div>
+        </div>
+        <div className="story">2</div>
+        <div className="personal">3</div>
+        <footer>4</footer>
       </div>
-    </main>
+    </>
   );
 }
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ const Card = () => {
   return (
     <div className="card">
       <img src="../images/img-1.jpg" alt="no pictu" />
-      <img src="../images/img-2.jpg" alt="no pictu" />
+      <img src="../images/img-2 .jpg" alt="no pictu" />
     </div>
   );
 };
